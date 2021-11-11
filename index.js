ae4619cc113d8a34c92a17771c5967d136e3b15f
shaksham08 <shakshamsinha@outlook.com>
2021-11-11 23:57:44 +0530

    Initialized first basic electron appllication

---
+++ index.js
@@ -0,0 +1,24 @@
+//importing electron library
+const electron = reuire("electron");
+
+const { app } = electron; //getting app property
+
+/*
+-----------electron process----------
+
+Electron starts
+
+app process is called  - take some amount of time
+
+app ready to start doing things - need to wait till here
+
+..
+..
+..
+app closes down
+*/
+
+//running when app is ready to start doing things
+app.on("ready", () => {
+  console.log("APp is now ready");
+});
