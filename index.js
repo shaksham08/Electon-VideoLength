bfb326e4daf26c847de1fdba24e32f6def5fbfd2
shaksham08 <shakshamsinha@outlook.com>
2021-11-12 16:14:02 +0530

    added code to send duration from electron to mainWindow and display

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //importing electron library
 const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
 
 const { app, BrowserWindow, ipcMain } = electron; //getting app property
 
+//if getting error make sure give proper ffmpeg path
+ffmpeg.setFfprobePath(__dirname + "//..//..//ffmpeg//ffprobe.exe");
+ffmpeg.setFfmpegPath(__dirname + "//..//..//ffmpeg//ffprobe.exe");
 /*
 -----------electron process----------
 
@@ -18,10 +22,23 @@ app ready to start doing things - need to wait till here
 app closes down
 */
 
+let mainWindow;
+
 //running when app is ready to start doing things
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({}); // start a new broswer window
+  // start a new broswer window
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on("video:submit", () => {});
+ipcMain.on("video:submit", (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log("The video duration is :- ", metadata.format.duration);
+    mainWindow.webContents.send("video:metadata", metadata.format.duration);
+  });
+});
